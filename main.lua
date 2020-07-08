283bd07feb7e000ca7d4e751cce0d3c3255201b7
ElderTwig <pqfrml@gmail.com>
2020-07-09 00:06:48 +0200

    Change G_Character and its derivatives to G_Rocket

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@ AnimLib = require("lib/animation")
 
 G_Framerate = 0
 
-G_CharacterHeight = 64
-G_CharacterWidth = 64
+G_RocketHeight = 64
+G_RocketWidth = 64
 
 G_GroundLevel = 400
 G_StartHeight = 400 - 64
@@ -21,7 +21,7 @@ function CreateFireAnimation(AnimLib)
     return animation
 end
 
-G_Character = 
+G_Rocket = 
         {pos_x = 100, pos_y = G_StartHeight,
          vel_x = 0, vel_y = 0, image = love.graphics.newImage("assets/rocket.png"),
          animation = CreateFireAnimation(AnimLib), isThrusting = false} 
@@ -37,58 +37,58 @@ function love.update(dt)
     G_Framerate = 1/dt	
 
     -- there are also callback functions that are called on key-presses
-    G_Character.isThrusting = love.keyboard.isDown("space")
+    G_Rocket.isThrusting = love.keyboard.isDown("space")
 
     -- Physics
-    G_Character.pos_x = G_Character.pos_x + G_Character.vel_x * dt
-    G_Character.pos_y = G_Character.pos_y + G_Character.vel_y * dt
+    G_Rocket.pos_x = G_Rocket.pos_x + G_Rocket.vel_x * dt
+    G_Rocket.pos_y = G_Rocket.pos_y + G_Rocket.vel_y * dt
 
     local g = -100
 
-    G_Character.vel_y = G_Character.vel_y - g*dt
+    G_Rocket.vel_y = G_Rocket.vel_y - g*dt
 
     -- Controls
-    if G_Character.isThrusting then
-            G_Character.vel_y = G_Character.vel_y + 2*g*dt
+    if G_Rocket.isThrusting then
+            G_Rocket.vel_y = G_Rocket.vel_y + 2*g*dt
     end
 
-    if love.keyboard.isDown("right") and G_Character.pos_y ~= G_StartHeight then
-        G_Character.vel_x = G_Character.vel_x + 5
+    if love.keyboard.isDown("right") and G_Rocket.pos_y ~= G_StartHeight then
+        G_Rocket.vel_x = G_Rocket.vel_x + 5
     end		
 
-    if love.keyboard.isDown("left") and G_Character.pos_y ~= G_StartHeight then
-        G_Character.vel_x = G_Character.vel_x - 5
+    if love.keyboard.isDown("left") and G_Rocket.pos_y ~= G_StartHeight then
+        G_Rocket.vel_x = G_Rocket.vel_x - 5
     end	
 
     -- More physics
-    G_Character.pos_x = G_Character.pos_x + G_Character.vel_x*dt
-    G_Character.pos_y = G_Character.pos_y + G_Character.vel_y*dt
+    G_Rocket.pos_x = G_Rocket.pos_x + G_Rocket.vel_x*dt
+    G_Rocket.pos_y = G_Rocket.pos_y + G_Rocket.vel_y*dt
 
-    if G_Character.pos_y > G_StartHeight then
-        G_Character.pos_y = G_StartHeight
-        G_Character.vel_y = 0
+    if G_Rocket.pos_y > G_StartHeight then
+        G_Rocket.pos_y = G_StartHeight
+        G_Rocket.vel_y = 0
     end
 
-    if G_Character.pos_y == G_StartHeight then
-        G_Character.vel_x = G_Character.vel_x * 0.95
+    if G_Rocket.pos_y == G_StartHeight then
+        G_Rocket.vel_x = G_Rocket.vel_x * 0.95
     end
 
-    if G_Character.pos_x > G_ScreenWidth + G_CharacterWidth then
-        G_Character.pos_x = -G_CharacterWidth
+    if G_Rocket.pos_x > G_ScreenWidth + G_RocketWidth then
+        G_Rocket.pos_x = -G_RocketWidth
     end		
 
-    if G_Character.pos_x < -G_CharacterWidth then
-            G_Character.pos_x = G_ScreenWidth + G_CharacterWidth
+    if G_Rocket.pos_x < -G_RocketWidth then
+            G_Rocket.pos_x = G_ScreenWidth + G_RocketWidth
     end		
 
     AnimLib.updateAllAnimations(dt)
 end
 
-function UpdateBackgroundColor(love, characterHeight, startHeight)
-    if characterHeight < -500 then
+function UpdateBackgroundColor(love, rocketHeight, startHeight)
+    if rocketHeight < -500 then
         love.graphics.setBackgroundColor(19/255, 20/255, 68/255)
     else
-        local r = (characterHeight + 500) / (500 + startHeight)
+        local r = (rocketHeight + 500) / (500 + startHeight)
         love.graphics.setBackgroundColor((1+2*r)*19/255, (1+2*r)*20/255, (1+2*r)*68/255)
     end
 end
@@ -97,26 +97,26 @@ function SetCameraPosition(love, x, y)
     love.graphics.translate(x, y)
 end
 
-function RenderCharacter(love, character)
-    love.graphics.draw(character.image, character.pos_x, character.pos_y)
+function RenderRocket(love, rocket)
+    love.graphics.draw(rocket.image, rocket.pos_x, rocket.pos_y)
 
-    if character.isThrusting then
+    if rocket.isThrusting then
         love.graphics.draw(
-                character.animation.image, character.animation.frames[math.floor(character.animation.curFrame)],
-                character.pos_x + 32 - 8, character.pos_y + 64 - 8)
+                rocket.animation.image, rocket.animation.frames[math.floor(rocket.animation.curFrame)],
+                rocket.pos_x + 32 - 8, rocket.pos_y + 64 - 8)
     end
 end
 
 function love.draw()
-    UpdateBackgroundColor(love, G_Character.pos_y, G_StartHeight)
+    UpdateBackgroundColor(love, G_Rocket.pos_y, G_StartHeight)
 
-    SetCameraPosition(love, 0, G_StartHeight - G_Character.pos_y)
+    SetCameraPosition(love, 0, G_StartHeight - G_Rocket.pos_y)
 
     -- The first argument is DrawMode, the other possibility is "line"
     -- for outlined shapes
     love.graphics.rectangle("fill", 0, G_GroundLevel, G_ScreenWidth, 80)
 
-    RenderCharacter(love, G_Character)
+    RenderRocket(love, G_Rocket)
 
     -- This resets the translation above
     -- so that we can draw GUI in screen space coordinates
@@ -127,4 +127,3 @@ function love.draw()
     -- math.floor(x + delta_x / 2)
     love.graphics.print({{1,0,0,1},math.floor(G_Framerate+0.5)}, 0, 0)
 end
-
